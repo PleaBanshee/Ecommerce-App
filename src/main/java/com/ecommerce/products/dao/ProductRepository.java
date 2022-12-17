44982eec8d03d79464f56c4660389c7191494a90
Plea_Banshee <llewellynant@gmail.com>
2022-12-17 21:57:46 +0200

    Added Query Method to search Products by Category

--- src/main/java/com/ecommerce/products/dao/ProductRepository.java
+++ src/main/java/com/ecommerce/products/dao/ProductRepository.java
@@ -2,7 +2,10 @@
 package com.ecommerce.products.dao;
 
 import com.ecommerce.products.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 // Interface extends the JpaRepository, receives a Product and ID (PK)
@@ -10,4 +13,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 @CrossOrigin("http://localhost:4200") // frontend
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
+    // Query Method: find a page (category) by ID. Spring Data REST will automatically expose the endpoint
+    // Example: http://localhost:8080/api/products/search/findByCategoryId?id=1
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
