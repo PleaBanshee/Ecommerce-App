45652907a03c292004422a3781e0a3626e050600
Plea_Banshee <llewellynant@gmail.com>
2022-12-17 11:09:09 +0200

    Added CORS to Product Repositories

--- src/main/java/com/ecommerce/products/dao/ProductCategoryRepository.java
+++ src/main/java/com/ecommerce/products/dao/ProductCategoryRepository.java
@@ -4,10 +4,12 @@ package com.ecommerce.products.dao;
 import com.ecommerce.products.entity.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 // customize export mapping and rels.
 // collectionResourceRel: rel value to use when generating links to the collection resource (name of JSON entry)
 // path: path segment under which this resource is to be exported (endpoint)
+@CrossOrigin("http://localhost:4200")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
